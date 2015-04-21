763e14cb60d1e5ba721bdcebc9d6b76937f2b99a
Gustav Fahl <gfahl67@yahoo.se>
2015-04-21 08:08:35 +0800

    "snake_case" <=> "CamelCase": let it also loop through "separate words"

--- Packages/User/toggle_camel_snake_case.py
+++ Packages/User/toggle_camel_snake_case.py
@@ -1,12 +1,16 @@
 import sublime, sublime_plugin, re, string
 
 class ToggleCamelSnakeCaseCommand(sublime_plugin.TextCommand):
+	# changes selection as follows:
+	# "foo fie fum" => "foo_fie_fum" => "FooFieFum" => "foo fie fum" etc.
     def run(self, edit):
         for rg in reversed(self.view.sel()):
             s = self.view.substr(rg)
             # if re.search("_", s):
             if s.find("_") >= 0:
                 s = "".join(map(string.capwords, s.split("_")))
+            elif s.find(" ") >= 0:
+                s = s.replace(" ", "_")
             else:
-                s = "_".join(map(string.lower, re.findall("[A-Z][a-z]*", s)))
+            	s = " ".join(map(string.lower, re.findall("[A-Z][a-z]*", s)))
             self.view.replace(edit, rg, s)
