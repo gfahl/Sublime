cd4323f43af6e69474fd24edfc86105a9ca50341
Gustav Fahl <gfahl67@yahoo.se>
2015-07-15 15:40:13 +0200

    get_multiple_cursors: if selection within one row, get cursors at all positions

--- Packages/User/get_multiple_cursors.py
+++ Packages/User/get_multiple_cursors.py
@@ -12,6 +12,19 @@ class GetMultipleCursorsCommand(sublime_plugin.TextCommand):
             self.view.run_command('split_selection_into_lines')
         elif has_bookmarks(self.view):
             self.view.run_command('select_all_bookmarks')
+        else:
+            new_sel = []
+            for rg in self.view.sel():
+                if rg.a != rg.b:
+                    pos = rg.begin()
+                    while pos < rg.end():
+                        new_sel.append(sublime.Region(pos, pos + 1))
+                        pos = pos + 1
+                else:
+                    new_sel.append(rg)
+            self.view.sel().clear()
+            for rg in new_sel:
+                self.view.sel().add(rg)
 
     def is_enabled(self):
-        return has_bookmarks(self.view) or has_selected_lines(self.view)
+        return True
