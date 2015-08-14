c3942ef2aacdb72d801bc728995f19d9214b22d1
Gustav Fahl <gfahl67@yahoo.se>
2015-08-14 13:06:00 +0800

    make move by paragraph work for multi-selection

--- Packages/User/move_by_paragraph.py
+++ Packages/User/move_by_paragraph.py
@@ -2,23 +2,31 @@ import sublime, sublime_plugin
 
 class MoveByParagraphCommand(sublime_plugin.TextCommand):
     def run(self, edit, extend = False, forward = True):
-        old_a = self.view.sel()[0].a
-        self.view.run_command("move_to", {"to": "hardbol", "extend": extend}) # to mimic TextPad's behaviour
-        pt = self.view.sel()[0].b
-        if forward:
-            rg = self.view.find("\n\s*\n", pt)
-            new_b = rg.b if rg else self.view.size()
-        else:
-            # couldn't find "find previous" command
-            rgs = self.view.find_all("\n[\s]*\n")
-            new_b = 0
-            for rg in rgs:
-                if rg.b < pt:
-                    new_b = rg.b
-        new_b_visible = self.view.visible_region().contains(new_b)
-        #
-        new_a = old_a if extend else new_b
+        new_sel = []
+        any_new_b_visible = False
+        fallback_center_pt = None
+
+        for rg in self.view.sel():
+            old_a = rg.a
+            old_b = rg.b
+            pt = self.view.line(rg.b).begin() # to mimic TextPad's behaviour
+            if forward:
+                _rg = self.view.find("\n\s*\n", pt)
+                new_b = _rg.b if _rg else self.view.size()
+            else:
+                # couldn't find "find previous" command
+                _rgs = self.view.find_all("\n[\s]*\n")
+                new_b = 0
+                for _rg in _rgs:
+                    if _rg.b < pt:
+                        new_b = _rg.b
+            if self.view.visible_region().contains(old_b):
+                fallback_center_pt = new_b
+            any_new_b_visible = any_new_b_visible or self.view.visible_region().contains(new_b)
+            new_a = old_a if extend else new_b
+            new_sel.append(sublime.Region(new_a, new_b))
         self.view.sel().clear()
-        self.view.sel().add(sublime.Region(new_a, new_b))
-        if not new_b_visible:
-            self.view.show_at_center(new_b)
+        for rg in new_sel:
+            self.view.sel().add(rg)
+        if not any_new_b_visible and fallback_center_pt != None:
+            self.view.show_at_center(fallback_center_pt)
