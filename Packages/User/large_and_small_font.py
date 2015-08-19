071e0bc840bafa2891a74311673060d8689d5c31
Gustav Fahl <gfahl67@yahoo.se>
2015-08-19 10:59:09 +0800

    new preference settings and commands for large_font_size, small_font_size
    new keyboard shortcuts:
    	[ctrl+q, ctrl+left] default font size
    	[ctrl+q, ctrl+right] large font size
    change [decrease/increase]_font_size to use ctrl+[down/up]

---
+++ Packages/User/large_and_small_font.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin
+
+class SmallFontSizeCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        s = sublime.load_settings("Preferences.sublime-settings")
+        size = s.get("small_font_size", 10)
+        s.set("font_size", size)
+        sublime.save_settings("Preferences.sublime-settings")
+
+class LargeFontSizeCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        s = sublime.load_settings("Preferences.sublime-settings")
+        size = s.get("large_font_size", 24)
+        s.set("font_size", size)
+        sublime.save_settings("Preferences.sublime-settings")
