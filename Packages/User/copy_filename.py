b3ee6dd45a7dca7ffbb55982c88e6208aa0ecaf1
Gustav Fahl <gfahl67@yahoo.se>
2015-08-27 08:14:29 +0800

    bugfix: Copy Filename command on Windows

--- Packages/User/copy_filename.py
+++ Packages/User/copy_filename.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin, re
 class CopyFilenameCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         s = self.view.file_name()
-        sublime.set_clipboard(re.match('.*/(.*)', s).group(1))
+        sublime.set_clipboard(re.match('.*[/\\\\](.*)', s).group(1))
         sublime.status_message("Copied file name")
 
     def is_enabled(self):
