ff7a4842b301a94161a54bdfc98bd20293434561
Gustav Fahl <gfahl67@yahoo.se>
2015-08-27 08:20:41 +0800

    new command: Toggle Visible Spaces

---
+++ Packages/User/toggle_visible_spaces.py
@@ -0,0 +1,11 @@
+import sublime, sublime_plugin
+
+class ToggleVisibleSpacesCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.active_view()
+        current_value = v.settings().get('draw_white_space')
+        if current_value == "selection":
+            new_value = "all"
+        else:
+            new_value = "selection"
+        v.settings().set('draw_white_space', new_value)
