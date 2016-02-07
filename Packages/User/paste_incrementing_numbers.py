5da7c7d931b93ea8c6feaec39053ea7b981f6ac6
Gustav Fahl <gfahl67@yahoo.se>
2016-02-07 16:15:19 +0800

    new command: paste incrementing numbers (prompting for start number)

--- Packages/User/paste_incrementing_numbers.py
+++ Packages/User/paste_incrementing_numbers.py
@@ -1,9 +1,16 @@
 import sublime, sublime_plugin
 
 class PasteIncrementingNumbersCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, start = 1):
         regions = self.view.sel()
-        n = len(regions)
+        n = start + len(regions) - 1
         for rg in reversed(regions):
             self.view.replace(edit, rg, str(n))
             n -= 1
+
+class PasteIncrementingNumbersInteractiveCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.show_input_panel("Start:", "", self.on_done, None, None)
+
+    def on_done(self, text):
+        self.window.active_view().run_command("paste_incrementing_numbers", {"start": int(text)} )
