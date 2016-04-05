c89ba69587479e4fd1648cdc9829ae96edcb5687
Gustav Fahl <gfahl67@yahoo.se>
2016-04-05 14:08:34 +0800

    add Ruler commands

---
+++ Packages/User/ruler_interactive.py
@@ -0,0 +1,9 @@
+import sublime, sublime_plugin
+
+class RulerInteractiveCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.show_input_panel("Column:", "", self.on_done, None, None)
+
+    def on_done(self, text):
+        columns = map(int, text.split(","))
+        self.window.active_view().run_command("set_setting", {"setting": "rulers", "value": columns} )
