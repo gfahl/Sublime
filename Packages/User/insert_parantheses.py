b139362ecf5ba048d2a9ec55762bc24f0c3dc6f8
Gustav Fahl <gfahl67@yahoo.se>
2016-04-24 17:06:20 +0800

    new commands: Insert or Modify Quotes/Parentheses

---
+++ Packages/User/insert_parantheses.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin, re, string
+
+class InsertParenthesesCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for rg in reversed(self.view.sel()):
+            s = self.view.substr(rg)
+            if s[0] == "(" and s[-1] == ")":
+                s = "[" + s[1:-1] + "]"
+            elif s[0] == "[" and s[-1] == "]":
+                s = "{" + s[1:-1] + "}"
+            elif s[0] == "{" and s[-1] == "}":
+                s = s[1:-1]
+            else:
+                s = "(" + s + ")"
+            self.view.replace(edit, rg, s)
