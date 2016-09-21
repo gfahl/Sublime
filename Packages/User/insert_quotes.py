712275b9ae7b3b13ec06d2d91a5974488979210b
AS\gfahl <gfahl67@yahoo.se>
2016-09-21 10:10:15 +0800

    insert parantheses/quotes: do a ctrl+d first if empty selection

--- Packages/User/insert_quotes.py
+++ Packages/User/insert_quotes.py
@@ -2,6 +2,8 @@ import sublime, sublime_plugin, re, string
 
 class InsertQuotesCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        if any(rg.empty() for rg in self.view.sel()):
+            self.view.window().run_command("find_under_expand")
         for rg in reversed(self.view.sel()):
             s = self.view.substr(rg)
             if s[0] == "'" and s[-1] == "'":
