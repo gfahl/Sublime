e5a4ed0aebfd4fe960400b682fcc4a4f7cf88a70
AS\gfahl <gfahl67@yahoo.se>
2016-10-16 16:02:20 +0800

    new commands: Multi-Select: Extend Upwards/Downwards
    replaces ctrl+alt+pageup/pagedown

---
+++ Packages/User/extend_multiselection.py
@@ -0,0 +1,18 @@
+import sublime, sublime_plugin, re
+
+class ExtendMultiselectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit, forward):
+        v = self.view
+        if forward:
+            rg = v.sel()[-1]
+            start_row, start_col = v.rowcol(rg.a)
+            end_row, end_col = v.rowcol(rg.b)
+            a = v.text_point(start_row + 1, start_col)
+            b = v.text_point(end_row + 1, end_col)
+        else:
+            rg = v.sel()[0]
+            start_row, start_col = v.rowcol(rg.a)
+            end_row, end_col = v.rowcol(rg.b)
+            a = v.text_point(start_row - 1, start_col)
+            b = v.text_point(end_row - 1, end_col)
+        v.sel().add(sublime.Region(a, b))
