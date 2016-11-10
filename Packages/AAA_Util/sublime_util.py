e111a674d394e1108127bd8259cf25712038cd27
AS\gfahl <gfahl67@yahoo.se>
2016-11-10 10:00:48 +0800

    utilities

--- Packages/AAA_Util/sublime_util.py
+++ Packages/AAA_Util/sublime_util.py
@@ -3,6 +3,15 @@ import sublime
 def col(self, text_point):
 	return self.rowcol(text_point)[1]
 
+def max_col(self, row):
+	if row < 0 or row > self.max_row():
+		return 0
+	else:
+		return self.col(self.line(self.text_point(row, 0)).end())
+
+def max_row(self):
+	return self.rowcol(self.size())[0]
+
 def row(self, text_point):
 	return self.rowcol(text_point)[0]
 
@@ -15,6 +24,8 @@ def set_selection(self, regions):
 	    self.sel().add(rg)
 
 sublime.View.rowcol_exists = rowcol_exists
+sublime.View.max_col = max_col
+sublime.View.max_row = max_row
 sublime.View.row = row
 sublime.View.col = col
 sublime.View.set_selection = set_selection
