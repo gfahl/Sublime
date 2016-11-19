be0d215ec31992727574bb8fea8fa85a9dcc9ab4
AS\gfahl <gfahl67@yahoo.se>
2016-11-19 13:14:32 +0800

    introduce syntax-specific fold; add it for gtxt

---
+++ Packages/GText/fold_by_level_gtext.py
@@ -0,0 +1,32 @@
+import sublime, sublime_plugin, re
+
+class FoldByLevelGtextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, level):
+        print 'hello from FoldByLevelGtextCommand %s' % level
+        v = self.view
+        folds = []
+        tp = 0
+        if level == 1:
+            regex = '[~]'
+        elif level == 2:
+            regex = '[~=]'
+        elif level >= 3:
+            regex = '[~=\-]'
+        while tp < v.size():
+            rg = v.full_line(tp)
+            s = v.substr(rg)
+            if re.match(regex, s):
+                if folds != [] and rg.a == folds[-1].b:
+                    folds[-1] = sublime.Region(folds[-1].a, folds[-1].b - 1)
+            else:
+                if folds != [] and rg.a == folds[-1].b:
+                    folds[-1] = sublime.Region(folds[-1].a, rg.b)
+                else:
+                    folds.append(rg)
+            tp = v.full_line(tp).b
+
+        v.run_command('unfold_all')
+        v.fold(folds)
+        v.show(v.sel())
+
+        sublime.status_message("Folded " + str(len(folds)) + " regions")
