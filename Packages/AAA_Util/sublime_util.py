5978f376f032815d1eacaaa5e6189b6acedbaf11
AS\gfahl <gfahl67@yahoo.se>
2016-11-23 19:20:21 +0800

    combine add & draw frame commands
    "Canvas: Draw Frame"
    ctrl+alt+f

--- Packages/AAA_Util/sublime_util.py
+++ Packages/AAA_Util/sublime_util.py
@@ -30,6 +30,147 @@ sublime.View.row = row
 sublime.View.col = col
 sublime.View.set_selection = set_selection
 
+def is_rectangular(self, v):
+    if len(set([(v.col(rg.begin()), v.col(rg.end())) for rg in self])) != 1:
+        # start columns, and end columns, have to be equal
+        return False
+    if v.col(rg.begin()) == v.col(rg.end()):
+        # width must be >= 1
+        return False
+    if list(set([v.row(rg.end()) - v.row(rg.begin()) for rg in self])) != [0]:
+        # no region can span more than one row
+        return False
+    if range(v.row(self[0].begin()), v.row(self[-1].begin()) + 1, 1) != [v.row(rg.begin()) for rg in self]:
+        # rows should be consecutive
+        return False
+    return True
+
+sublime.RegionSet.is_rectangular = is_rectangular
+
 def to_snake_case(s):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+class Rect:
+    directions = tuple('WNES')
+
+    @classmethod
+    def from_selection(klass, v):
+        left = min(map(lambda rg: v.col(rg.begin()), v.sel()))
+        top = v.row(v.sel()[0].begin())
+        right = max(map(lambda rg: v.col(rg.end()), v.sel()))
+        bottom = v.row(v.sel()[-1].end())
+        return klass(v, (left, top, right, bottom))
+
+    def __init__(self, v, pos_as_tuple):
+        self.v = v
+        self.pos = {
+            'W': pos_as_tuple[0],
+            'N': pos_as_tuple[1],
+            'E': pos_as_tuple[2],
+            'S': pos_as_tuple[3]
+            }
+
+    def __str__(self):
+        return "<Rect: (%d, %d) (%d, %d)>" % (self.pos_as_tuple())
+
+    def copy(self):
+        return Rect(self.v, self.pos_as_tuple())
+
+    def draw_frame(self, edit, vertical):
+        line = '+' + '-' * (self.width() - 2)
+        if self.width() > 1: line += '+'
+        self.v.replace(edit, sublime.Region(self.text_point('NW'), self.text_point('NE')), line)
+        for row in range(self.pos['N'] + 1, self.pos['S']):
+            tp1 = self.v.text_point(row, self.pos['W'])
+            tp2 = self.v.text_point(row, self.pos['E'])
+            rg = sublime.Region(tp1, tp2)
+            old_text = self.v.substr(rg)
+            new_text = vertical + old_text[1:-1]
+            if self.width() > 1: new_text += '|'
+            self.v.replace(edit, rg, new_text)
+        self.v.replace(edit, sublime.Region(self.text_point('SW'), self.text_point('SE')), line)
+        self.use_as_selection()
+
+    def expand(self, direction):
+        if direction not in Rect.directions:
+            raise Exception('Unexpected direction: %s' % direction)
+        delta = -1 if direction == 'W' or direction == 'N' else 1
+        self.pos[direction] += delta
+        return self
+
+    def height(self):
+        return self.pos['S'] - self.pos['N'] + 1
+
+    def is_frame(self):
+        return (
+            self.is_line('W') and
+            self.is_line('N') and
+            self.is_line('E') and
+            self.is_line('S')
+            )
+
+    def is_horizontal_line(self, row, left_col, right_col):
+        s = ''
+        col = left_col
+        while col <= right_col:
+            s += self.v.substr(self.v.text_point(row, col))
+            col += 1
+        return re.match('[+-]+$', s)
+
+    def is_line(self, side):
+        if side == 'W':
+            return self.is_vertical_line(self.pos['W'], self.pos['N'], self.pos['S'])
+        elif side == 'E':
+            return self.is_vertical_line(self.pos['E'] - 1, self.pos['N'], self.pos['S'])
+        elif side == 'N':
+            return self.is_horizontal_line(self.pos['N'], self.pos['W'], self.pos['E'] - 1)
+        elif side == 'S':
+            return self.is_horizontal_line(self.pos['S'], self.pos['W'], self.pos['E'] - 1)
+        else:
+            raise Exception('Unexpected side: %s' % side)
+
+    def is_valid(self):
+        return (
+            self.v.rowcol_exists(self.pos['N'], self.pos['W']) and
+            self.v.rowcol_exists(self.pos['N'], self.pos['E']) and
+            self.v.rowcol_exists(self.pos['S'], self.pos['W']) and
+            self.v.rowcol_exists(self.pos['S'], self.pos['E'])
+            )
+
+    def is_vertical_line(self, col, top_row, bottom_row):
+        s = ''
+        row = top_row
+        while row <= bottom_row:
+            s += self.v.substr(self.v.text_point(row, col))
+            row += 1
+        return re.match('[+|]+$', s)
+
+    def pos_as_tuple(self):
+        return (self.pos['W'], self.pos['N'], self.pos['E'], self.pos['S'])
+
+    def size(self):
+        return self.height() * self.width()
+
+    def text_point(self, corner):
+        if corner == 'NW':
+            return self.v.text_point(self.pos['N'], self.pos['W'])
+        elif corner == 'NE':
+            return self.v.text_point(self.pos['N'], self.pos['E'])
+        elif corner == 'SE':
+            return self.v.text_point(self.pos['S'], self.pos['E'])
+        elif corner == 'SW':
+            return self.v.text_point(self.pos['S'], self.pos['W'])
+        else:
+            raise Exception('Unexpected corner: %s' % side)
+
+    def use_as_selection(self):
+        regions = []
+        for row in range(self.pos['N'], self.pos['S'] + 1, 1):
+            a = self.v.text_point(row, self.pos['W'])
+            b = self.v.text_point(row, self.pos['E'])
+            regions.append(sublime.Region(a, b))
+        self.v.set_selection(regions)
+
+    def width(self):
+        return self.pos['E'] - self.pos['W']
--- Packages/Canvas/draw_frame.py
+++ Packages/Canvas/draw_frame.py
@@ -1,40 +1,32 @@
 import sublime, sublime_plugin, re
+import sublime_util as su
 
 class DrawFrameCommand(sublime_plugin.TextCommand):
-    # Draw an ascii frame on the edges of selected text
-    # Pre-requisite: a rectangular-shaped multi-selection
-    #   the selections are on consecutive lines
-    #   the selections have the same start- and end-position within the lines
-    # The command does not add any new lines or columns
-    # Height of one line and/or width of one column is allowed
-    # If a drawn line passes an orthogonal line, an intersection is drawn
-    #
-    #   abc <suppose      > jkl        abc +-------------+ jkl
-    #   def <this text was> mno   =>   def |this text was| mno
-    #   ghi <selected     > pqr        ghi +-------------+ pqr
-    def run(self, edit):
-        rows_a = [self.view.rowcol(r.a)[0] for r in self.view.sel()]
-        rows_b = [self.view.rowcol(r.b)[0] for r in self.view.sel()]
-        cols_a = [self.view.rowcol(r.a)[1] for r in self.view.sel()]
-        cols_b = [self.view.rowcol(r.b)[1] for r in self.view.sel()]
-        if rows_a != rows_b: return
-        if rows_a != range(rows_a[0], rows_a[-1] + 1): return
-        if len(set(cols_a)) != 1: return
-        if len(set(cols_b)) != 1: return
-        if cols_a == cols_b: return
-        for i in [0, -1]:
-            region = self.view.sel()[i]
-            old_text = self.view.substr(region)
-            new_text = re.sub("\|", "+", old_text)
-            new_text = re.sub("[^+]", "-", new_text)
-            new_text = "+" + new_text[1:]
-            new_text = new_text[:-1] + "+"
-            self.view.replace(edit, region, new_text)
-        if len(self.view.sel()) > 2:
-            for i in range(1, len(self.view.sel()) - 1):
-                region = self.view.sel()[i]
-                old_text = self.view.substr(region)
-                new_text = old_text
-                new_text = ("+" if old_text[0] == "+" or old_text[0] == "-" else "|") + new_text[1:]
-                new_text = new_text[:-1] + ("+" if old_text[0] == "+" or old_text[0] == "-" else "|")
-                self.view.replace(edit, region, new_text)
+    # Draws an ascii frame
+    def run(self, edit, vertical = '|'):
+        v = self.view
+        sel = v.sel()
+        if len(sel) == 1 and v.line(sel[0].a) != v.line(sel[0].b):
+            # Add one line above & below, and two 'columns' left & right; then draw the frame
+            #
+            # <BOL>abc <suppose          <BOL>+-----------------+
+            # <BOL>this text was     =>  <BOL>| abc suppose     |
+            # <BOL>selected> ghijkl      <BOL>| this text was   |
+            #                            <BOL>| selected ghijkl |
+            #                            <BOL>+-----------------+
+            p1 = v.line(sel[0].begin()).a
+            v.insert(edit, p1, '\n')
+            p2 = v.line(sel[0].end() - 1).b
+            v.insert(edit, p2, '\n')
+            v.set_selection([sublime.Region(p1, p2 + 2)])
+            self.view.run_command('right_pad_spaces')
+            v.set_selection(v.lines(sel[0]))
+            for rg in sel:
+                v.replace(edit, rg, '  ' + v.substr(rg) + '  ')
+            su.Rect.from_selection(v).draw_frame(edit, vertical)
+            v.set_selection([sublime.Region(sel[0].a, sel[-1].b + 1)])
+        elif v.sel().is_rectangular(v):
+            # <BOL>abc <suppose      > jkl       <BOL>abc +-----------+ jkl
+            # <BOL>def <this text was> mno   =>  <BOL>def |his text wa| mno
+            # <BOL>ghi <selected     > pqr       <BOL>ghi +-----------+ pqr
+            su.Rect.from_selection(v).draw_frame(edit, vertical)
