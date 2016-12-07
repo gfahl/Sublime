1f49857347631577964a6be226a09409597da1ae
AS\gfahl <gfahl67@yahoo.se>
2016-12-07 10:27:02 +0800

    make move/select by word work with japanese text

---
+++ Packages/User/move_by_word.py
@@ -0,0 +1,21 @@
+import sublime, sublime_plugin, re
+
+class MoveByWordCommand(sublime_plugin.TextCommand):
+    def run(self, edit, forward = True, extend = False):
+        delta = 1 if forward else -1
+        v = self.view
+        new_sel = []
+
+        for rg in v.sel():
+            old_a = rg.a
+            old_b = rg.b
+            line_extremity = v.line(old_b).end() if forward else v.line(old_b).begin()
+            if old_b == line_extremity:
+                new_b = old_b + delta
+            else:
+                s = v.substr(sublime.Region(old_b, line_extremity))[::delta]
+                m = re.search(r'(\S\s|\w[^\w\s]|[^\w\s]\w)', s, re.UNICODE)
+                new_b = old_b + (m.start() + 1) * delta if m else line_extremity
+            new_a = old_a if extend else new_b
+            new_sel.append(sublime.Region(new_a, new_b))
+        v.set_selection(new_sel)
