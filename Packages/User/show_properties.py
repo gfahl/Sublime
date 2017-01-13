aef06590792c150967b47770a9fad2a0a2b9fe0d
AS\gfahl <gfahl67@yahoo.se>
2017-01-13 11:25:50 +0800

    bugfix show_properties: handle non-saved buffers

--- Packages/User/show_properties.py
+++ Packages/User/show_properties.py
@@ -5,15 +5,20 @@ class ShowPropertiesCommand(sublime_plugin.WindowCommand):
     def run(self):
         v = self.window.active_view()
         s = ""
-        m = re.match('(.*)[/\\\\](.*)', v.file_name())
-        s += "Filename: %s\n" % m.group(2)
-        s += "Directory: %s\n" % m.group(1)
+        if v.file_name():
+            m = re.match('(.*)[/\\\\](.*)', v.file_name())
+            s += "Filename: %s\n" % m.group(2)
+            s += "Directory: %s\n" % m.group(1)
+        else:
+            s += "Filename: n/a\n"
+            s += "Directory: n/a\n"
         s += "Encoding: %s\n" % v.encoding()
         s += "Tab Size: %s\n" % v.settings().get('tab_size')
         s += "Indentation: %s\n" % ("Spaces" if v.settings().get('translate_tabs_to_spaces') else "Tabs")
         s += "Size: %d characters\n" % v.size()
         s += "Line Endings: %s\n" % v.line_endings()
-        s += "Syntax file: %s\n" % v.settings().get('syntax')
+        s += "Syntax File: %s\n" % v.settings().get('syntax')
+        s += "Color Scheme: %s\n" % v.settings().get('color_scheme')
         s += "Font: %s %dpt\n" % (v.settings().get('font_face'), v.settings().get('font_size'))
         sublime.message_dialog(s)
         print s
