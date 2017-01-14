699a94bce46358abd43f2f53576dc1d796c1ffa8
AS\gfahl <gfahl67@yahoo.se>
2017-01-14 11:17:20 +0800

    add info to Show Properties: Selection, Scope at point

--- Packages/User/show_properties.py
+++ Packages/User/show_properties.py
@@ -16,8 +16,13 @@ class ShowPropertiesCommand(sublime_plugin.WindowCommand):
         s += "Tab Size: %s\n" % v.settings().get('tab_size')
         s += "Indentation: %s\n" % ("Spaces" if v.settings().get('translate_tabs_to_spaces') else "Tabs")
         s += "Size: %d characters\n" % v.size()
+        s += "Selection: %s\n" % v.sel()
         s += "Line Endings: %s\n" % v.line_endings()
         s += "Syntax File: %s\n" % v.settings().get('syntax')
+        if v.empty_selection():
+            s += "Scope at point: %s\n" % v.scope_name(v.sel()[0].a)
+        else:
+            s += "Scope at point: n/a\n"
         s += "Color Scheme: %s\n" % v.settings().get('color_scheme')
         s += "Font: %s %dpt\n" % (v.settings().get('font_face'), v.settings().get('font_size'))
         sublime.message_dialog(s)
